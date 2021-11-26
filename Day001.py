70bb233ef8486829571f5cbba20e0e7317afbbe9
ostamo1 <ostamo1@gmail.com>
2021-11-25 23:34:49 -0500

    Day 1 in 100days of code in python where i made a basic band generator

---
+++ Day001.py
@@ -0,0 +1,14 @@
+#!/bin/python
+from os import system
+system("cls")
+####################################
+
+print("Welcome to the Band Name Generator.")
+#city = input("What is the name of the city you grew up in?")
+#petname = input("What is your Pet's name?")
+#bandname = city + ' ' + petname
+#print(f"Your Band Name could be the {bandname} ")
+print("Your Band Name could be the "+input("What is the name of the city you grew up in?") + " " + input("What is your Pet's name?"))
+
+####################################
+print("\n\n")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 100daysofcode
 my 100 days of python course
+Day 1 of #100DaysofCode
+Starting 100 days of python so i can become a better programmer
+and learn Python and complete projects
+
+
